3fef81b8bb14ee457cb6857fb119a626235561b3
Mandeep Singh <mandeeparora132@gmail.com>
2025-02-02 22:58:30 +0530

    grind day1: array_sorted_and_rotated

---
+++ array_sorted_and_rotated.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def check(self, nums: List[int]) -> bool:
+        n = len(nums)
+        count = 1
+
+        for i in range(1, 2 * n):
+            if nums[(i - 1) % n] <= nums[i % n]:
+                count +=1
+            else:
+                count = 1
+            if count == n:
+                return True
+
+        return n == 1
+
+solution = Solution()
+print(solution.check([3,4,5,1,2]))
