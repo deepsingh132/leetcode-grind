72b764ba1d5434d096ced300a5f08efb4476ebea
Mandeep Singh <mandeeparora132@gmail.com>
2025-02-16 23:59:25 +0530

    grind day5: #49 groupAnagrams

---
+++ groupAnagrams.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+  def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    res = defaultdict(list)
+
+    for s in strs:
+      count = [0] * 26
+
+      for c in s:
+        count[ord(c) - ord('a')] += 1
+      res[tuple(count)].append(s)
+    return list(res.values())
+
+solution = Solution()
+print(solution.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
